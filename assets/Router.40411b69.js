3ff0b716ee2c6d74dda5b58681b7a1c28d86869e
ZarZaKos <gaston.campana.piarrou@gmail.com>
2022-03-01 23:15:00 -0300

    Updates

--- assets/Router.40411b69.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.b769a9fc.js";import{N as r,o as t,g as c}from"./vendor.727e917b.js";const n={};function _(s,a){const e=r("router-view");return t(),c(e)}var p=o(n,[["render",_]]);export{p as default};
