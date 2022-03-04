4bc5fba1a945b9cba82be9cbb83530c2341e91cd
ZarZaKos <gaston.campana.piarrou@gmail.com>
2022-03-03 22:47:28 -0300

    Updates

--- assets/Router.c3c9e660.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";import{Y as r,o as t,h as a,w as n,j as c,Z as s}from"./vendor.cc39b7be.js";const _={};function f(i,u){const e=r("router-view");return t(),a(s,{fluid:""},{default:n(()=>[c(e)]),_:1})}var d=o(_,[["render",f]]);export{d as default};
