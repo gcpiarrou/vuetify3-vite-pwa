95d396ec70b418e8665bd42e147ac2a6c8f27778
ZarZaKos <gaston.campana.piarrou@gmail.com>
2022-03-03 22:47:47 -0300

    Added not found page with 404 message

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container class="py-6 py-lg-12 text-center">
+    <h1 class="text-h4 text-sm-h3 text-md-h2 text-lg-h1">404</h1>
+    <h2 class="text-h6 text-sm-h5 secondary--text mt-4 w-full w-md-8-12 w-xl-half mx-auto">Page not found</h2>
+  </v-container>
+</template>
+
+<script setup>
+
+</script>
